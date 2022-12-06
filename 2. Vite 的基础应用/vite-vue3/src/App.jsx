0f69076213f0e9e4f6adc93e97af162cfa2ac8fe
zrtch <1160639597@qq.com>
2022-12-06 17:29:21 +0800

    feat:vite增加静态资源

--- 2. Vite 的基础应用/vite-vue3/src/App.jsx
+++ 2. Vite 的基础应用/vite-vue3/src/App.jsx
@@ -2,11 +2,20 @@ import { defineComponent } from 'vue'
 // import '@styles/index.css'
 import '@styles/test.less'
 import classes from '@styles/test.module.css'
+import logo from './assets/vue.svg'
+
+import test from './main?raw'
+console.log(test) // /src/main.js
 
 export default defineComponent({
   setup() {
     return () => {
-      return <div class={`root ${classes.moduleClass}`}>hello vue jsx</div>
+      return (
+        <>
+          <div class={`root ${classes.moduleClass}`}>hello vue jsx</div>
+          <img src={logo} alt="" />
+        </>
+      )
     }
   },
 })
--- 2. Vite 的基础应用/vite-vue3/src/main.js
+++ 2. Vite 的基础应用/vite-vue3/src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App'
+import Worker from "./worker?worker";
+import pkg from '../package.json';
+console.log('🤩  pkg', pkg);
+
+// const worker = new Worker();
+
+// worker.onmessage = function (e) {
+//   console.log(e);
+// };
+
 
 createApp(App).mount('#app')
