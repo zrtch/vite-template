f5c7f3d5fb96cc93725f64e1d51cc340091ddc8a
zrtch <1160639597@qq.com>
2022-12-07 15:39:14 +0800

    feat:配置env

---
+++ 2. Vite 的基础应用/vite-env/src/main.ts
@@ -0,0 +1,26 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import { setupCounter } from './counter'
+
+console.log(import.meta.env);
+const x = import.meta.env.VITE_TITLE // 通过vit-env.d.ts文件是可以拿到类型
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
