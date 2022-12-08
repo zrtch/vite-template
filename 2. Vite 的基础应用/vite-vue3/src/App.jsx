e17d1ecc6340fd55f9eedd98c5d2c176887d66ea
zrtch <1160639597@qq.com>
2022-12-08 14:51:53 +0800

    feat:glob-import

--- 2. Vite 的基础应用/vite-vue3/src/App.jsx
+++ 2. Vite 的基础应用/vite-vue3/src/App.jsx
@@ -1,24 +1,25 @@
-import { defineComponent } from 'vue'
+import { defineComponent } from 'vue';
 // import '@styles/index.css'
-import '@styles/test.less'
-import classes from '@styles/test.module.css'
-import logo from './assets/vue.svg'
+import '@styles/test.less';
+import classes from '@styles/test.module.css';
+import logo from './assets/vue.svg';
 
-import main from './main?raw'
-console.log(main) // /src/main.js
+import main from './main?raw';
+// console.log(main); // /src/main.js
 
-import { a } from './test'
-console.log('🤩  a', a)
+// import { a } from './test'
+// console.log('🤩  a', a)
 
 export default defineComponent({
   setup() {
     return () => {
       return (
         <>
-          <div class={`root ${classes.moduleClass}`}>hello {a.name}</div>
+          <div class={`root ${classes.moduleClass}`}>hello vite</div>
           <img src={logo} alt="" />
+          <p>111</p>
         </>
-      )
-    }
+      );
+    };
   },
-})
+});
