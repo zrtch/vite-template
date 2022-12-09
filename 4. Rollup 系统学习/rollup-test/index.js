b788e261bb6dc3f4326429d3ae92edb55a407d37
zrtch <1160639597@qq.com>
2022-12-09 15:40:12 +0800

    feat:rollup配置项

--- 4. Rollup 系统学习/rollup-test/index.js
+++ 4. Rollup 系统学习/rollup-test/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { funA } from './a'
 
 funA()
--- 4. Rollup 系统学习/rollup-test/rollup.config.js
+++ 4. Rollup 系统学习/rollup-test/rollup.config.js
@@ -1,10 +1,29 @@
-const mode = process.env.MODE
-console.log(mode);
-// export default {
-//   input: 'index.js',
-//   output: {
-//     file: "dist.js",
-//     format: 'umd',
-//     name: 'Index'
-//   },
-// }
+import json from '@rollup/plugin-json'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+
+export default [
+  {
+    input: 'index.js',
+    external: ['react'], // external作用就是有些包我们不想让他打包进去，就可以设置
+    plugins: [resolve(), commonjs(), json()],
+    output: {
+      file: 'dist/index.es.js', // 指定输出目录dist
+      format: 'es',
+      plugins: [terser()], // 在output也可以使用plugins，terser的作用就是压缩代码
+      banner: "/** hello this is banner **/" // 注释的作用
+    },
+  },
+  {
+    input: 'index.js',
+    plugins: [resolve(), commonjs(), json()],
+    external: ['react'],
+    output: {
+      file: 'dist/index.umd.js',
+      format: 'umd',
+      name: 'Index',
+      banner: "/** hello this is banner **/" // 注释的作用
+    },
+  },
+]
